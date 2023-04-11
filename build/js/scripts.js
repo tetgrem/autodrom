e91397d1c19fd1a66b020464c23b17c2383d59d1
tetgrem <tetgrem@gmail.com>
2023-04-12 00:49:51 +0300

    buyProcess section ready

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -57,6 +57,25 @@ function fixedHeader() {
 }
 // window.addEventListener('scroll', fixedHeader)
 
+// Аккордеон
+function accordion() {
+    const items = document.querySelectorAll('.accordion__item-trigger')
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            const parent = item.parentNode
+            if (parent.classList.contains('accordion__item-active')) {
+                parent.classList.remove('accordion__item-active')
+            } else {
+                document
+                    .querySelectorAll('.accordion__item')
+                    .forEach(child => child.classList.remove('accordion__item-active'))
+                parent.classList.add('accordion__item-active')
+            }
+        })
+    })
+}
+accordion()
+
 
 const swiper = new Swiper('.whatcars-swiper', {
 
